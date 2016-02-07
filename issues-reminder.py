f909252655df7d601db8bb1f431f0350c1f83600
Thomas Papillon <tp@irrealis.me>
2016-02-07 10:33:38 +0100

    Introduce basic scheduling

--- issues-reminder.py
+++ issues-reminder.py
@@ -1,3 +1,7 @@
+import schedule
+import time
+import yaml
+
 import senders
 import fetchers
 import reminder
@@ -8,4 +12,22 @@ if __name__ == '__main__':
         senders_clss=[senders.SlackSender, senders.StdOutSender],
         fetcher_clss=[fetchers.GitHubFetcher]
     )
-    r.run()
+
+    settings = yaml.load(open('settings.yaml'))
+    ev = settings['freq']['every']
+    unit = settings['freq']['unit']
+
+    if unit == 'seconds':
+        schedule.every(ev).seconds.do(r.run)
+    elif unit == 'minutes':
+        schedule.every(ev).minutes.do(r.run)
+    elif unit == 'hours':
+        schedule.every(ev).hours.do(r.run)
+    elif unit == 'days':
+        schedule.every(ev).days.do(r.run)
+    else:
+        raise Exception('Bad time unit in settings.yaml')
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
